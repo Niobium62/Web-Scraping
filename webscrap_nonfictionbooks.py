bf2038497b1ef4fe009541645bd30741f572354b
Niobium62 <47160603+Niobium62@users.noreply.github.com>
2023-09-18 20:42:33 -0400

    Nonfiction Webscraping
    Made the script output a csv containing nonfiction data

--- webscrap_nonfictionbooks.py
+++ webscrap_nonfictionbooks.py
@@ -1,7 +1,7 @@
 #This script will scrap data from all nonfiction books from toscrape.com.
 
-
 import requests as rq
+import csv
 from bs4 import BeautifulSoup as bs
 
 def extract_book_data(book):
@@ -20,6 +20,10 @@ def extract_book_data(book):
     return my_list
 
 def main():
+    file = open('export_data.csv', 'w', newline='')
+    writer = csv.writer(file)
+    headers = ['Title', 'Rating', 'Price']
+    writer.writerow(headers)
     for page_num in range (1,7):
         URL = "http://books.toscrape.com/catalogue/category/books/nonfiction_13/page-" + str(page_num) + ".html"
         page = rq.get(URL)
@@ -29,11 +33,12 @@ def main():
 
         for book in books:
             book_data = extract_book_data(book)
+            writer.writerow(book_data)
             print("Title:\t" + book_data[0])
             print("Rating:\t" + book_data[1])
             print("Price:\t" + book_data[2])
             print()
-
+    file.close()
 main()
 
     
