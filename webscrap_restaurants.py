ab4eaf0eb1b2e51a6ae618c9cfd3a2058a27dd24
Niobium62 <47160603+Niobium62@users.noreply.github.com>
2023-12-27 15:55:25 -0500

    fixed pagination info not being taken from webpage

--- webscrap_restaurants.py
+++ webscrap_restaurants.py
@@ -3,6 +3,7 @@
 import requests as rq
 import csv
 from bs4 import BeautifulSoup as bs
+import re
 
 CSV_HEADERS = ['Restaurant Name', 'Rating', 'Tags', 'Price Range']
 BASE_URL = "https://www.yelp.com/search?find_desc=&find_loc=North+York%2C+Toronto%2C+Ontario&start={}"
@@ -28,12 +29,16 @@ def extract_data(restaurant):
     return [name, rating, tags, price_range]
 
 def get_num_pages():
-    #get num_pages from the site
+    #request url
     url = BASE_URL.format(0)
     page = rq.get(url, headers=USER_AGENT)
     soup = bs(page.content, "html.parser")
-    pagination_info = soup.find(class_='pagination__09f24__VRjN4 css-1dhksiy')
-    pagination_info = pagination_info.find(class_="css-1aq64zd").text
+    #pagination_info = soup.find(class_='pagination__09f24__VRjN4 css-1dhksiy')
+    #find the element that contains the info for pagination
+    pagination_info = soup.find_all('div', class_=lambda value: value and value.startswith("pagination__09f24"))
+    print (pagination_info is None)
+    #find the number of pages within that element
+    pagination_info = pagination_info[0].find(class_="css-1aq64zd").text
     num_pages = pagination_info.split(" ")
     num_pages = int(num_pages[len(num_pages)-1])
     return num_pages
@@ -57,7 +62,8 @@ if __name__ == "__main__":
         restaurants = soup.find_all(class_="css-ady4rt")
 
         #go through each restaurant, excluding the sponsored ones
-        for j in range (2, len(restaurants)-1):
+        #the first eight on each page are sponsored. skip those
+        for j in range (8, len(restaurants)-1):
             NUM = NUM + 1
             restaurant = restaurants[j]
             #extract data from each restaurant including name, rating, etc
